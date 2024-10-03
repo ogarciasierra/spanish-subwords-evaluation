4ec466160e213dc445d1c8f92a6c2959ad37b313
ogarciasierra <oscar.garcia@medlabmg.com>
2024-10-03 08:09:15 +0200

    add beto path

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from src.morphological_evaluator import MorphologicalEvaluator
 
 if __name__ == "__main__":    
-    tokenizers = ["subword_tokenizers_31/wordpiece"]
+    tokenizers = ["dccuchile/bert-base-spanish-wwm-uncased"]
     evaluator = MorphologicalEvaluator(tokenizers_paths=tokenizers)
     evaluator.full_eval()
